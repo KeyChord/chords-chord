54ff9b5c8481fd8311dedca8dfeb1b7736bed16c
Leon Si <leon@leonsilicon.com>
2026-04-02 23:35:16 +0000

    format
    Committed via a GitHub action: https://github.com/KeyChord/chords/actions/runs/23926910225
    
    Co-authored-by: leonsilicon <leonsilicon@users.noreply.github.com>

---
+++ js/package.json.js
@@ -0,0 +1,11 @@
+//#region package.json
+var name = "@keychord/chords-chord";
+var type = "module";
+var devDependencies = { "@keychord/config": "catalog:" };
+var package_default = {
+	name,
+	type,
+	devDependencies
+};
+//#endregion
+export { package_default as default, devDependencies, name, type };
